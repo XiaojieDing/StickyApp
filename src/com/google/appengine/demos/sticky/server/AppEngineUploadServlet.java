d749b20f5d98b28c800cccdec0b5972e91094dbf
Claus Polanka <claus.polanka@chello.at>
2010-12-28 16:19:08 +0100

    Extended NoteView with a gwtupload-library based SingleUpload.

---
+++ src/com/google/appengine/demos/sticky/server/AppEngineUploadServlet.java
@@ -0,0 +1,8 @@
+package com.google.appengine.demos.sticky.server;
+
+import gwtupload.server.gae.AppEngineUploadAction;
+
+@SuppressWarnings("serial")
+public class AppEngineUploadServlet extends AppEngineUploadAction {
+
+}
